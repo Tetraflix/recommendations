bd1d96af47609cc7bb1c83532dd5da39e8cd0990
Shweta Patrachari <shwetachari@gmail.com>
2017-10-23 16:47:00 -0700

    modified redic client to use process.env.PORT if defined

--- database/movies/index.js
+++ database/movies/index.js
@@ -1,13 +1,14 @@
 const redis = require('redis');
 
-const redisClient = redis.createClient();
+const port = process.env.PORT || 6379;
+const redisClient = redis.createClient({ host: 'localhost', port });
 
 redisClient.on('ready', () => {
-  console.log('Redis Client connected');
+  console.log(`Redis Client connected on Port ${port}`);
 });
 
 redisClient.on('error', (err) => {
-  console.error('Error connected Redis Client', err);
+  console.error(`Error connected Redis Client to Port ${port}`, err);
 });
 
 module.exports = redisClient;
