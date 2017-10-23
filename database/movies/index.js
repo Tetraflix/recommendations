23a25152b8488e7afcd5ff8adc063769d9d7a6b7
Shweta Patrachari <shwetachari@gmail.com>
2017-10-23 16:51:39 -0700

    modified app / redis connection for eslint

--- database/movies/index.js
+++ database/movies/index.js
@@ -3,10 +3,6 @@ const redis = require('redis');
 const port = process.env.PORT || 6379;
 const redisClient = redis.createClient({ host: 'localhost', port });
 
-redisClient.on('ready', () => {
-  console.log(`Redis Client connected on Port ${port}`);
-});
-
 redisClient.on('error', (err) => {
   console.error(`Error connected Redis Client to Port ${port}`, err);
 });
--- server/index.js
+++ server/index.js
@@ -6,4 +6,5 @@ const port = 3000;
 
 app.listen(port, () => {
   console.log(`App is listening on Port ${port}!`);
+  console.log(`Redis Client is listening on Port ${redisClient.options.port}!`);
 });
